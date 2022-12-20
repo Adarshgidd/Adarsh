78e6da59730d4c916b973222ee52618f5a13ea12
Dell <Dell@DESKTOP-767SROO>
2022-12-20 18:07:42 +0530

    first Commit

---
+++ ATM/src/my/atm/Customer_Data.java
@@ -0,0 +1,12 @@
+package my.atm;
+
+/**
+ *
+ * @author HP
+ */
+public class Customer_Data {
+
+    public int accountNum, startingBalance = 0, accountNum1;
+    public String pincode = "", name = "", type = "", pincode1 = "", name1 = "", login = "", login1 = "",date;
+    public Object status;
+}
